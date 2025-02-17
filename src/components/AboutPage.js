b113d1fb2df6c32d7fc6790cf9488152ebb36ac8
cimatujv <cimatujoel87@gmail.com>
2025-02-17 23:19:45 +0800

    hotel selection design code

---
+++ src/components/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div className="about-page">
+      <h1>About Us</h1>
+      <p>Learn more about the Hotel Chatbot and our mission.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
