d2773cb85cc2ee79b78319d1fdeb62992785dc6f
Lance Darrick Melendrez <melendrezlance49@gmail.com>
2025-02-18 08:26:35 +0800

    fix the file issue conflict

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";  
 import "../styling/Auth.css";
-import { useNavigate } from "react-router-dom";  // Import useNavigate
 
 const Auth = ({ onLogin }) => {
   const [isRegister, setIsRegister] = useState(false);
